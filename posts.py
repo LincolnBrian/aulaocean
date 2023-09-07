6e87577d8e69b8e8b069be7e8d2d5cbb5e54863f
Lincoln Brian <brianlincolnbento@gmail.com>
2023-09-07 01:29:36 +0000

    ainda completando

---
+++ posts.py
@@ -0,0 +1,10 @@
+posts = [
+    {
+        "titulo": "Minha primeira postagem",
+        "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra."
+    },
+    {
+        "titulo": "Segundo Post",
+        'texto': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra."
+    }
+]
