7bda227de05bf2b1ad43c7f18a5f5466e650cdbb
Lincoln Brian <brianlincolnbento@gmail.com>
2023-09-11 23:46:09 +0000

    feat: Adicionados tabela de posts e base de dados

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, abort,  session, flash, redirect, url_for
+from flask import Flask, render_template, request, session, abort, flash, redirect, url_for
 from posts import posts
-app = Flask("Meu post")
-app.config['SECRET_KEY'] = 'pudin'
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pudim'
 
 @app.route('/')
 def exibir_entradas():
@@ -14,10 +14,10 @@ def login():
     erro = None
     if request.method == "POST":
         if request.form['username'] == "admin" and request.form['password'] == "admin":
-            session["logado"] = True
+            session['logado'] = True
             flash("Login efetuado com sucesso!")
             return redirect(url_for('exibir_entradas'))
-        erro = "Usuário ou senha inválidos"
+        erro = "Usuário ou senha inválidos"        
     return render_template('login.html', erro=erro)
 
 @app.route('/logout')
@@ -29,18 +29,20 @@ def logout():
 @app.route('/inserir', methods=["POST"])
 def inserir_entradas():
     if session['logado']:
-        novo_post =  {
+        novo_post = {
             "titulo": request.form['titulo'],
             "texto": request.form['texto']
         }
         posts.append(novo_post)
-        flash('Post criando com sucesso!')
+        flash("Post criado com sucesso!")
     return redirect(url_for('exibir_entradas'))
 
     @app.route('/posts/<int:id>')
     def exibir_entrada(id):
-        try: 
-            entrada = posts[id-1]
+       try: 
+           entrada = posts[id-1]
             return render_template('exibir_entrada.html', entrada=entrada)
-        except Exception:
-            return abort(404)
+       except Exception:
+           return abort(404)
+
+
